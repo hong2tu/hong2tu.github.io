683bfa97a82fd778c15cc18d3f770b2d89f49b2d
hong2tu <58047668+hong2tu@users.noreply.github.com>
2020-09-23 17:42:53 -0400

    fixes

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+    res.send('hello world')
   });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
