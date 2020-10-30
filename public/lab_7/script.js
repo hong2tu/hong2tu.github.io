307ab21fb6e8a3c6159241e5b7936557bbfc6a50
hong2tu <58047668+hong2tu@users.noreply.github.com>
2020-10-30 18:40:55 -0400

    updates

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,19 +1,50 @@
 function convertRestaurantsToCategories(restaurantList) {
+  console.log('memes');
   // process your restaurants here!
-  return list;
+  const allCategories = restaurantList.map((restaurant) => restaurant.category); // ['abc', 'def', 'abc', 'abc', 'xyz']
+  const categoryMap = {};
+
+  allCategories.forEach((category) => {
+    if (categoryMap.hasOwnProperty(category)) {
+      categoryMap[category] += 1;
+    } else {
+      categoryMap[category] = 1;
+    }
+  });
+
+  console.log(categoryMap);
+
+  const restaurantArray = [];
+
+  Object.keys(categoryMap).forEach((category) => {
+    restaurantArray.push({ label: category, y: categoryMap[category] });
+  });
+
+  console.log(restaurantArray);
+
+  return restaurantArray;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
   CanvasJS.addColorSet('customColorSet1', [
+    [
+      '#7fffd4',
+      '#000000',
+      '#0000ff',
+      '#00ffff',
+      '#ff8c00',
+      '#e0ffff',
+      '#191970'
+    ]
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
   ]);
 
   return {
     animationEnabled: true,
-    colorSet: 'customColorSet1',
+    colorSet: 'colorSet1',
     title: {
-      text: 'Change This Title'
+      text: 'Places To Eat Out In Future'
     },
     axisX: {
       interval: 1,
@@ -22,9 +53,29 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
       gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      title: 'Restaurants by Category',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {
+        customBreaks: [
+          {
+            startValue: 40,
+            endValue: 50,
+            color: 'orange',
+            type: 'zigzag'
+          },
+          {
+            startValue: 85,
+            endValue: 100,
+            color: 'orange',
+            type: 'zigzag'
+          },
+          {
+            startValue: 140,
+            endValue: 175,
+            color: 'orange',
+            type: 'zigzag'
+          },
+      ]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 import countries from './public/lab_6/countries.js';
+
 
 dotenv.config();
 
@@ -25,10 +27,16 @@ app.route('/api')
     console.log('GET request detected');
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    res.json(countries);
+
+    const graph = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const graphJson = await graph.json();
+
+    console.log(graphJson);
+
+    res.json(graphJson);
   });
 
 
